25c41b64cfe511763fd0d4667701a96f2fe3babc
ysbllcby <ysabellacubay@gmail.com>
2023-04-20 12:23:24 -0600

    ok.

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,6 +13,10 @@ public class Calculator implements ActionListener {
     JPanel panel;
 
     Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
     Calculator() {
     }
 
